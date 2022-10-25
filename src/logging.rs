3f496f77fb5fb8315cda3812eb9b0ca88a69832f
Ivan De Marino <ivan.de.marino@gmail.com>
2022-10-25 19:09:44 +0100

    Every logging level can now be supported
    `WARN` is default, going negative enables to select `ERROR` and `OFF`

--- src/logging.rs
+++ src/logging.rs
@@ -4,14 +4,17 @@ pub const LOG_FILTER_ENV_VAR: &str = "KSUNAMI_LOG";
 ///
 /// If the env var `KSUNAMI_LOG` is set, that will take precedence and configuration
 /// will be based on the rules described [here](https://docs.rs/env_logger/latest/env_logger/#enabling-logging).
-pub fn init(verbosity_level: u8) {
+pub fn init(verbosity_level: i8) {
     let default_log_level = match verbosity_level {
-        0 => log::Level::Info,
-        1 => log::Level::Debug,
-        _ => log::Level::Trace,
+        i8::MIN..=-2 => "OFF",
+        -1 => log::Level::Error.as_str(),
+        0 => log::Level::Warn.as_str(),
+        1 => log::Level::Info.as_str(),
+        2 => log::Level::Debug.as_str(),
+        3..=i8::MAX => log::Level::Trace.as_str(),
     };
 
-    let logger_env = env_logger::Env::default().filter_or(LOG_FILTER_ENV_VAR, default_log_level.as_str());
+    let logger_env = env_logger::Env::default().filter_or(LOG_FILTER_ENV_VAR, default_log_level);
     let mut logger_builder = env_logger::Builder::from_env(logger_env);
     logger_builder.init();
 
